0ae67fb7c4340894bd44960566a4c4d04d182e6b
taracamilleri <91306189+taracamilleri@users.noreply.github.com>
2023-11-26 10:01:02 +1100

    Whac-A-Mole starter code

---
+++ Javascript/5. Whac-A-Mole/app.js
@@ -0,0 +1,68 @@
+// Focus on timing events!
+// Overall objective: build a grid that displays a mole in random squares
+// If the user clicks on the mole they get a point
+
+// Change the class of one of the squares to 'square mole' directly in the html to see the objective
+
+// Select all the squares and the mole so we can use them in our JS file
+
+
+
+// Select the time left and score so we can use them in our JS file
+// Use the # selector for elements with an ID
+
+
+
+// Initialise the result to 0
+
+
+// GET THE MOLE TO APPEAR IN A RANDOM SQUARE
+let hitPosition;
+function randomSquare() {
+    // Remove the mole if it exists on any of the squares
+
+
+
+
+    // Add a mole to a random square
+
+
+    // Everytime you refresh the page you should see the mole in a random square
+    // Save the id of the square with the mole (so that we can later use this to see if the user clicks the correct square)
+
+}
+
+// MOVE THE MOLE BETWEEN RANDOM SQUARES
+// Create a function that calls the randomSquare function every 500ms
+// Use setInterval(function, time)
+function moveMole() {
+
+}
+
+// If you load the page you will see the mole jump around in random squares
+moveMole();
+
+// INCREMENT THE USERS SCORE
+// Each time the user clicks the mole you want to increment the score
+squares.forEach(square => {
+
+
+
+
+
+
+
+})
+
+// DECREASE THE TIMER
+
+function countDown() {
+
+
+
+
+
+
+
+}
+
